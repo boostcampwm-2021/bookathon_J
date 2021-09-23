a00327053c2aa8815d4476881c27da27e8b966df
tyehooney <xogns4313@naver.com>
2021-09-23 23:08:06 +0900

    feat: edit layout (LoginFragment)

--- app/src/main/java/com/tyehooney/fedyourpet/ui/LoginViewModel.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/LoginViewModel.kt
@@ -1,6 +1,11 @@
 package com.tyehooney.fedyourpet.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
+
+    private val _phoneNum = MutableLiveData<String>()
+    val phoneNum: LiveData<String> = _phoneNum
 }

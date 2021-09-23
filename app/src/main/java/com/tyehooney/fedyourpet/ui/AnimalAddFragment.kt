ceaab2110c984fd21c994cdf3ba13c7944f0f391
K007_김수명 <admin@sumyeong.kim>
2021-09-23 23:10:54 +0900

    [FEAT] Add animal profile picture choose feature

--- app/src/main/java/com/tyehooney/fedyourpet/ui/AnimalAddFragment.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/AnimalAddFragment.kt
@@ -1,14 +1,22 @@
 package com.tyehooney.fedyourpet.ui
 
+import android.app.Activity
 import android.app.TimePickerDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.tyehooney.fedyourpet.R
 import com.tyehooney.fedyourpet.databinding.FragmentAnimalAddBinding
 import java.util.*
@@ -21,6 +29,26 @@ class AnimalAddFragment : Fragment(R.layout.fragment_animal_add) {
     private val timeListLiveData: MutableLiveData<List<Pair<Int, Int>>> by lazy {
         MutableLiveData()
     }
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val imageURI = data?.data
+                Glide.with(this).asBitmap().load(imageURI).override(300, 300).fitCenter()
+                    .into(object : CustomTarget<Bitmap>() {
+                        override fun onResourceReady(
+                            resource: Bitmap,
+                            transition: Transition<in Bitmap>?,
+                        ) {
+                            //resource: 불러온 이미지 비트맵
+                            binding.fragmentAnimalAddImage.setImageBitmap(resource)
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                        }
+                    })
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +60,9 @@ class AnimalAddFragment : Fragment(R.layout.fragment_animal_add) {
             fragmentAnimalAddButtonTime.setOnClickListener {
                 addTime()
                 Log.d("times", timeList.toString())
+            }
+            fragmentAnimalAddImage.setOnClickListener {
+                chooseImage()
             }
         }
         return view
@@ -64,5 +95,12 @@ class AnimalAddFragment : Fragment(R.layout.fragment_animal_add) {
             }, hourOfDay, minute, true)
         timePickerDialog.setMessage("밥 주는 시간")
         timePickerDialog.show()
+    }
+
+    private fun chooseImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        resultLauncher.launch(intent)
     }
 }

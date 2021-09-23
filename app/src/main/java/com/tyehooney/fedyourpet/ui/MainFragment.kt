40ce792d7b1cc6066a79a30389a42eda8d3266a0
K007_김수명 <admin@sumyeong.kim>
2021-09-23 23:43:34 +0900

    [FEAT] Navigation graph

---
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.tyehooney.fedyourpet.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tyehooney.fedyourpet.R
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}

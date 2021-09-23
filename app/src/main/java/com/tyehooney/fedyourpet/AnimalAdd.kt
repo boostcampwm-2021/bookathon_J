0571e9f723b18b27b0bc3b4edace6099e86ec22d
K007_김수명 <admin@sumyeong.kim>
2021-09-23 19:02:10 +0900

    [CHORE] build.gradle 삭제

--- app/src/main/java/com/tyehooney/fedyourpet/AnimalAdd.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/AnimalAdd.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tyehooney.fedyourpet.databinding.FragmentAnimalAddBinding
 import java.util.*
 
@@ -15,6 +17,9 @@ class AnimalAdd : Fragment(R.layout.fragment_animal_add) {
     private var _binding: FragmentAnimalAddBinding? = null
     private val binding get() = _binding!!
     private val timeList = mutableListOf<Pair<Int, Int>>()
+    private val timeListLiveData: MutableLiveData<List<Pair<Int, Int>>> by lazy {
+        MutableLiveData()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +36,17 @@ class AnimalAdd : Fragment(R.layout.fragment_animal_add) {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = TimeListApapter()
+        val recyclerView = binding.fragmentAnimalAddTimeview
+        timeListLiveData.observe(viewLifecycleOwner, {
+            adapter.submitList(it.toList())
+        })
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -43,6 +59,7 @@ class AnimalAdd : Fragment(R.layout.fragment_animal_add) {
         val timePickerDialog =
             TimePickerDialog(context, TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
                 timeList.add(Pair(i, i2))
+                timeListLiveData.value = timeList
             }, hourOfDay, minute, true)
         timePickerDialog.setMessage("밥 주는 시간")
         timePickerDialog.show()
---
+++ app/src/main/java/com/tyehooney/fedyourpet/TimeListApapter.kt
@@ -0,0 +1,47 @@
+package com.tyehooney.fedyourpet
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.tyehooney.fedyourpet.databinding.TimelistItemBinding
+
+class TimeListApapter : ListAdapter<Pair<Int, Int>, TimeListApapter.TimeListViewHolder>(diffUtil) {
+    inner class TimeListViewHolder(binding: TimelistItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val hourOfDay = binding.hourOfDay
+        val minute = binding.minute
+        fun bind(pair: Pair<Int, Int>) {
+            hourOfDay.text = pair.first.toString()
+            minute.text = pair.second.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeListViewHolder {
+        val binding =
+            TimelistItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TimeListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TimeListViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Pair<Int, Int>>() {
+            override fun areItemsTheSame(
+                oldItem: Pair<Int, Int>,
+                newItem: Pair<Int, Int>
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Pair<Int, Int>,
+                newItem: Pair<Int, Int>
+            ): Boolean =
+                oldItem == newItem
+        }
+    }
+}

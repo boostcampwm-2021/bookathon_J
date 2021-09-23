39af56794727dcd2109025123d185e4398fc9d28
K043_이승수 <52225690+lee-seung-su@users.noreply.github.com>
2021-09-23 19:08:15 +0900

    Revert "[K043] - feed pet xml작성"

--- app/src/main/java/com/tyehooney/fedyourpet/MainActivity.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/MainActivity.kt
@@ -7,9 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_layout, FeedPet())
-        transaction.commit()
     }
 }

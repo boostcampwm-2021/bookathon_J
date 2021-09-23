9c8d6a9e35a3f492ee7f7ddd7eb9c1820037d0c4
cha-ji <chajiwon100785@gmail.com>
2021-09-24 00:07:47 +0900

    feat: profile recycler view

--- app/src/main/java/com/tyehooney/fedyourpet/ui/ProfileFragment.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/ProfileFragment.kt
@@ -6,42 +6,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.tyehooney.fedyourpet.R
+import com.tyehooney.fedyourpet.databinding.ProfileFragmentBinding
 
 class ProfileFragment : Fragment() {
-    private lateinit var viewModel: ProfileViewModel
+    private var _binding: ProfileFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var profileRecyclerView: RecyclerView
+    private var profileAdapter: ProfileAdapter? = null
+    private val profileViewModel: ProfileViewModel by lazy {
+        ViewModelProvider(this).get(ProfileViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.profile_fragment, container, false)
+    ): View {
+        _binding = ProfileFragmentBinding.inflate(inflater, container, false)
+        profileRecyclerView = binding.recyclerProfile
+        updateUI()
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+    /**
+     * Recycler view
+     */
+
+    private fun updateUI() {
+        val profiles = profileViewModel.profiles
+        profileAdapter = ProfileAdapter(profiles)
+        profileRecyclerView.adapter = profileAdapter
     }
 
     private inner class ProfileViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         private lateinit var family: Family
-//        var profileIcon: Icon = view.findViewById(R.id.)
+        val icon: Button = itemView.findViewById(R.id.icon_profile)
 
         init {
-            // TODO: on click
-            // 프로필 선택, 프로필 추가
-
+            icon.setOnClickListener {
+                // TODO: navigate 할 때 구성원 누군지 정보 넘겨줌
+                Toast.makeText(context, "${family.name}", Toast.LENGTH_SHORT).show() // FIXME
+            }
         }
 
-
         fun bind(family: Family) {
-            // val name = family.name
-            // val name
+            this.family = family
+            icon.text = family.name
 
+            // 추가 버튼
+            if (family.id == -1) {
+                icon.setOnClickListener {
+                    // TODO: 추가 팝업 생성
+                    Toast.makeText(context, "${family.name}", Toast.LENGTH_SHORT).show() // FIXME
+                    profileViewModel.profiles.add(Family())
+                }
+            }
         }
     }
 
@@ -53,7 +81,16 @@ class ProfileFragment : Fragment() {
         }
 
         override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
-            holder.bind(profiles[position])
+            val profile = profiles[position]
+
+            holder.bind(profile)
+
+            if (position == profiles.size - 1) {
+                val emptyFamily = Family()
+                emptyFamily.id = -1
+                emptyFamily.name = "+"
+                holder.bind(emptyFamily)
+            }
         }
 
         override fun getItemCount(): Int = profiles.size
@@ -63,5 +100,3 @@ class ProfileFragment : Fragment() {
         fun newInstance() = ProfileFragment()
     }
 }
-
-class Family // TODO
--- app/src/main/java/com/tyehooney/fedyourpet/ui/ProfileViewModel.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/ProfileViewModel.kt
@@ -2,6 +2,24 @@ package com.tyehooney.fedyourpet.ui
 
 import androidx.lifecycle.ViewModel
 
+// FIXME: Dummy
+class Family {
+    var id = 1
+    var name = "아빠"
+}
+
 class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val profiles = mutableListOf<Family>()
+
+    init {
+        // Dummy
+        val a = Family()
+        a.name = "아빠"
+
+        val b = Family()
+        b.name = "엄마"
+
+        profiles.add(a)
+        profiles.add(b)
+    }
 }

33aa6720c8d997d22ac5f2d51b3f82ca952f0fcb
tyehooney <xogns4313@naver.com>
2021-09-23 23:08:06 +0900

    feat: edit layout (LoginFragment)

--- app/src/main/java/com/tyehooney/fedyourpet/ui/LoginFragment.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/LoginFragment.kt
@@ -6,26 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.tyehooney.fedyourpet.R
+import com.tyehooney.fedyourpet.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
     private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: LoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding = LoginFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding.viewModel = viewModel
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 }

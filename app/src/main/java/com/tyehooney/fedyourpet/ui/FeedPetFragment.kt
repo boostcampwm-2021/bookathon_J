de9b223bc29b0bc8c1eebf728332ae35c01a6e5e
cha-ji <chajiwon100785@gmail.com>
2021-09-24 05:06:10 +0900

    fix: no use code

--- app/src/main/java/com/tyehooney/fedyourpet/ui/FeedPetFragment.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/FeedPetFragment.kt
@@ -11,8 +11,6 @@ import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.tyehooney.fedyourpet.databinding.FragmentFeedPetBinding

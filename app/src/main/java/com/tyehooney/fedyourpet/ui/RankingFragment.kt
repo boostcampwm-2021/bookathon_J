68cfc6c89ccc09c4b0b0c6f34671520932827e41
K007_김수명 <admin@sumyeong.kim>
2021-09-24 14:00:07 +0900

    [FEAT] Add dynamic ranking

--- app/src/main/java/com/tyehooney/fedyourpet/ui/RankingFragment.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/ui/RankingFragment.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.LegendEntry
@@ -22,75 +23,40 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.tyehooney.fedyourpet.R
 import com.tyehooney.fedyourpet.databinding.FragmentRankingBinding
+import com.tyehooney.fedyourpet.model.FeedLog
 import com.tyehooney.fedyourpet.model.Pet
+import com.tyehooney.fedyourpet.util.getAllLog
 import com.tyehooney.fedyourpet.util.getMyPets
 import com.tyehooney.fedyourpet.util.getProfiles
 
 class RankingFragment : Fragment(R.layout.fragment_ranking), RankingListener {
     private var _binding: FragmentRankingBinding? = null
     private val binding get() = _binding!!
-    private val memberList =
-        listOf(Member("아빠", 1, 2), Member("엄마", 2, 3), Member("아들", 3, 5), Member("딸", 4, 2))
+    private val memberMap = mutableMapOf<String, Member>()
     private val entries = arrayListOf<BarEntry>()
-    private val profiles = mutableListOf<String>()
+    val labels = mutableSetOf<String>()
+    lateinit var chart: BarChart
+    private val pets = mutableListOf<Pet>()
     private lateinit var sharedPreferences: SharedPreferences
+    private val colors = listOf(Color.GREEN, Color.YELLOW, Color.BLUE, Color.CYAN)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRankingBinding.inflate(inflater, container, false)
         val view = binding.root
-        val chart = binding.rankingBarchart
+        chart = binding.rankingBarchart
         sharedPreferences = requireActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE)
         getProfsAndPets()
-        val labels = arrayListOf<String>()
-        memberList.forEachIndexed { idx, it ->
-            entries.add(
-                BarEntry(
-                    (idx).toFloat() + 0.5F,
-                    floatArrayOf(it.dogFood.toFloat(), it.catFood.toFloat())
-                )
-            )
-            labels.add(it.name)
-        }
-        val dataSet = BarDataSet(entries, "밥 준 사람")
-        dataSet.colors = listOf(Color.GREEN, Color.YELLOW)
-        val barData = BarData(dataSet)
-        chart.data = barData
-        chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-        chart.xAxis.setCenterAxisLabels(true)
-        chart.xAxis.granularity = 1F
-        chart.xAxis.valueFormatter = MyXAxisFormatter()
-        chart.legend.setCustom(
-            listOf<LegendEntry>(
-                LegendEntry(
-                    "개",
-                    Legend.LegendForm.DEFAULT,
-                    Float.NaN,
-                    Float.NaN,
-                    null,
-                    Color.GREEN
-                ),
-                LegendEntry(
-                    "고양이",
-                    Legend.LegendForm.DEFAULT,
-                    Float.NaN,
-                    Float.NaN,
-                    null,
-                    Color.YELLOW
-                )
-            )
-        )
-        chart.animateY(1400, Easing.EaseOutBounce)
-        chart.invalidate()
+        Log.d("entry", entries.toString())
         return view
     }
 
     private fun getProfsAndPets() {
         val uid = sharedPreferences.getString("uid", null)
-        uid?.let {
-            getProfiles(it, this)
-            getMyPets(it, this)
+        uid?.let { userId ->
+            getProfiles(userId, this)
+            getMyPets(userId, this)
         }
     }
 
@@ -99,33 +65,100 @@ class RankingFragment : Fragment(R.layout.fragment_ranking), RankingListener {
         _binding = null
     }
 
-    inner class MyXAxisFormatter : ValueFormatter() {
-        private val days = mutableListOf<String>()
-
-        init {
-            memberList.forEach {
-                days.add(it.name)
-            }
-        }
-
-        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-            return days.getOrNull(value.toInt()) ?: value.toString()
-        }
-    }
 
     override fun onProfileReceived(profiles: List<String>) {
-        this.profiles.addAll(profiles)
-        Log.d("profiles", this.profiles.toString())
+        profiles.forEach {
+            memberMap[it] = Member(it)
+        }
     }
 
     override fun onGetMyPetsSuccess(pets: List<Pet>) {
-        Log.d("pets", pets.toString())
+        this.pets.addAll(pets)
+        Log.d("pets", this.pets.toString())
+        this.pets.forEach {
+            memberMap.values.forEach { member ->
+                member.feedNums[it.name] = 0
+
+            }
+            getAllLog(it.name, it.id, this)
+        }
+
+    }
+
+    private fun drawChart() {
+        val dataSet = BarDataSet(entries, "밥 준 사람")
+        dataSet.colors = colors
+        val barData = BarData(dataSet)
+        chart.data = barData
+        chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        chart.xAxis.setCenterAxisLabels(true)
+        chart.xAxis.setAvoidFirstLastClipping(true)
+        chart.xAxis.granularity = 1F
+        chart.xAxis.setLabelCount(labels.size, true)
+        chart.xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        chart.xAxis.axisMinimum = 0F
+//        chart.legend.setCustom(
+//            listOf<LegendEntry>(
+//                LegendEntry(
+//                    "개",
+//                    Legend.LegendForm.DEFAULT,
+//                    Float.NaN,
+//                    Float.NaN,
+//                    null,
+//                    Color.GREEN
+//                ),
+//                LegendEntry(
+//                    "고양이",
+//                    Legend.LegendForm.DEFAULT,
+//                    Float.NaN,
+//                    Float.NaN,
+//                    null,
+//                    Color.YELLOW
+//                )
+//            )
+//        )
+        chart.animateY(1400, Easing.EaseOutBounce)
+        chart.invalidate()
     }
 
     override fun onGetMyPetsFailed(msg: String) {
         Toast.makeText(context, "error : $msg", Toast.LENGTH_SHORT).show()
     }
+
+    override fun onGetLogsSuccess(logs: List<FeedLog>, petName: String) {
+        if (logs.isNotEmpty()) {
+            logs.forEach {
+                when (val count = memberMap[it.who]?.feedNums?.get(petName)) {
+                    null -> memberMap[it.who]?.feedNums?.set(petName, 1)
+                    else -> memberMap[it.who]?.feedNums?.set(petName, count + 1)
+                }
+            }
+            Log.d("logs", memberMap.toString())
+        }
+        memberMap.values.forEachIndexed { idx, member ->
+            entries.add(
+                BarEntry(
+                    idx.toFloat() + 0.5F,
+                    member.feedNums.toList().map { floatMap -> floatMap.second.toFloat() }
+                        .toFloatArray()
+                )
+            )
+            labels.add(member.name)
+            Log.d("label", labels.toString())
+        }
+        Log.d("entry", entries.toString())
+        drawChart()
+    }
+
+
+    override fun onGetLogsFailed(msg: String) {
+        Toast.makeText(context, "error: $msg", Toast.LENGTH_SHORT).show()
+    }
 }
 
-class Member(val name: String, val dogFood: Int, val catFood: Int) {
+class Member(val name: String) {
+    val feedNums = linkedMapOf<String, Int>()
+    override fun toString(): String {
+        return "$name: $feedNums"
+    }
 }

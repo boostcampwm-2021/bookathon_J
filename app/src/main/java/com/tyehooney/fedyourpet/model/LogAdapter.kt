8518a8bb515622d42b414987788d5d07e18b7710
tyehooney <xogns4313@naver.com>
2021-09-24 11:55:40 +0900

    feat: edit LogAdapter

--- app/src/main/java/com/tyehooney/fedyourpet/model/LogAdapter.kt
+++ app/src/main/java/com/tyehooney/fedyourpet/model/LogAdapter.kt
@@ -7,17 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.tyehooney.fedyourpet.R
+import java.text.SimpleDateFormat
 
-class LogAdapter(val context: Context) : RecyclerView.Adapter<ViewHolder>() {
-    lateinit var data: MutableList<String>
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+class LogAdapter(val context: Context) : RecyclerView.Adapter<FeedLogViewHolder>() {
+    lateinit var data: MutableList<FeedLog>
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedLogViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_log, parent, false)
-        return ViewHolder(view)
+        return FeedLogViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holderFeedLog: FeedLogViewHolder, position: Int) {
         val item = data.get(position)
-        holder.bind(item)
+        holderFeedLog.bind(item)
     }
 
     override fun getItemCount(): Int {
@@ -25,29 +26,13 @@ class LogAdapter(val context: Context) : RecyclerView.Adapter<ViewHolder>() {
     }
 }
 
-class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class FeedLogViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     var tvWho : TextView = view.findViewById(R.id.text_who)
     var tvWhen : TextView = view.findViewById(R.id.text_when)
 
-    fun bind(item: String){
-        var ind = -1
-        var hour = 0;
-        var min = 0
-        var sec = 0
-        for(i in 0..item.length - 1) {
-            if (item[i] == ','){
-                ind = i
-                break
-            }
-        }
-        tvWho.setText(item.substring(0, ind))
-        ind++
-        hour = item.substring(ind, ind+ 2).toInt()
-        ind += 3
-        min = item.substring(ind, ind + 2).toInt()
-        ind += 3
-        sec = item.substring(ind, ind + 2).toInt()
-        tvWhen.setText("${hour}시${min}분${sec}초")
+    fun bind(item: FeedLog){
+        tvWho.text = item.who
+        val dateFormat = SimpleDateFormat("HH시 mm분")
+        tvWhen.text = dateFormat.format(item.feedAt)
     }
-
 }
